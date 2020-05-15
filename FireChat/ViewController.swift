5190588aa9856c5b0f5be252c76daf81c65472c7
William Yeung <williamyeung1141998@gmail.com>
2020-05-15 16:30:14 -0400

    Initial project

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by William Yeung on 5/15/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
